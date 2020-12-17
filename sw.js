cc23d3636eb289830a73b07131630cd31825c28f
khobar <khobar@pop-os.localdomain>
2020-12-18 00:17:29 +0700

    init

---
+++ sw.js
@@ -0,0 +1,9 @@
+ //cek
+ self.addEventListener('install',e=>{
+     console.log('sw is installed')
+ })
+
+//activate process
+ self.addEventListener('activate',e=>{
+     console.log('sw is activated')
+ })
